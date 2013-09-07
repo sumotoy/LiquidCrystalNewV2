0e75dc2ff7553c4139b75c9fb46350e61ba14d94
max mc costa <sumotoy@gmail.com>
2013-09-08 02:16:41 +0300

    Update LiquidCrystalSSPI.cpp

--- LiquidCrystalSSPI.cpp
+++ LiquidCrystalSSPI.cpp
@@ -137,7 +137,7 @@ void LiquidCrystalSSPI::initChip(uint8_t dotsize, byte enPin) {
 void LiquidCrystalSSPI::send(uint8_t value, byte mode) {
 	byte en = _en1;
 	if (_multipleChip && _chip) en = _en2;
-	delayMicroseconds(DELAYPERCHAR);
+	//delayMicroseconds(DELAYPERCHAR);
 	setDataMode(mode);					// I2C & SPI
 		bitWrite(_theData,LCDPIN_D4,value & 0x10);
 		bitWrite(_theData,LCDPIN_D5,value & 0x20);
