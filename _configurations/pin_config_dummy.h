85dd676dd9947d430115816d624f69a6ce07af19
sumotoy <sumotoy@gmail.com>
2013-09-25 15:09:11 +0200

    a little note...

--- _configurations/pin_config_dummy.h
+++ _configurations/pin_config_dummy.h
@@ -8,7 +8,10 @@
 		#include <Arduino.h>
 	#endif
 #endif
-
+/*
+	in direct connection mode it's no needed a specific configuration file since pins are defined directly by the instance.
+	this dummy file contains the only parameter you can configure....
+*/
 #define BACKGND_LGHTINV	//depends of what you using for drive led background. In my case I use an p-channel mosfet called IRFD9120
 
 #endif
